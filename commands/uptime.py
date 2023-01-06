7ed7a7fea58bba21a873a397a254ac5012ae40c9
YrDaddy <84126221+QuietArtX@users.noreply.github.com>
2023-01-06 21:57:34 +0700

    Create uptime.py

---
+++ commands/uptime.py
@@ -0,0 +1,17 @@
+@quiet.command()
+async def uptime(ctx):
+    await ctx.message.delete()
+    current_time = time.time()
+    uptime = int(current_time - start_time)
+    uptime_minutes, uptime_seconds = divmod(uptime, 60)
+    uptime_hours, uptime_minutes = divmod(uptime_minutes, 60)
+    uptime_days, uptime_hours = divmod(uptime_hours, 24)
+    message = f'I have been running for '
+    if uptime_days > 0:
+        message += f'{uptime_days} days, '
+    if uptime_hours > 0:
+        message += f'{uptime_hours} hours, '
+    if uptime_minutes > 0:
+        message += f'{uptime_minutes} minutes, '
+    message += f'and {uptime_seconds} seconds.'
+    await ctx.send(message)
