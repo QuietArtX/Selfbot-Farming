5aba5827ae132eb6754d6bbf4437d4228cc98260
YrDaddy <84126221+QuietArtX@users.noreply.github.com>
2023-01-08 15:42:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ quiet = commands.Bot(command_prefix = get_prefix, self_bot=True, help_command=No
 
 @quiet.command()
 async def setstr(ctx):
-    activity = discord.Streaming(platform="Youtube", name="Streaming with image", url="!!play https://www.youtube.com/watch?v=92izkAK5OA0&feature=youtu.be", detail="SUUUI", state="APA IYAH?", assets=Activity.assets.larga_image("https://cdn.discordapp.com/attachments/1061025685419851849/1061307014162161694/20230107_130234.png"))
+    activity = discord.Streaming(platform="Youtube", name="Streaming with image", url="!!play https://www.youtube.com/watch?v=92izkAK5OA0&feature=youtu.be", detail="SUUUI", state="APA IYAH?", assets=assets.large_image("https://cdn.discordapp.com/attachments/1061025685419851849/1061307014162161694/20230107_130234.png"))
     await quiet.change_presence(activity=activity)
 
 #--------------------------------------------
