0d1d0c3dda742c29bc3384193abf9d9d05d11140
YrDaddy <84126221+QuietArtX@users.noreply.github.com>
2023-01-10 15:46:01 +0700

    Create file_handling.py

---
+++ quiet/allcmd/file_handling.py
@@ -0,0 +1,10 @@
+@quiet.event
+async def on_ready():
+    # Load the prefix from the prefix file if exists
+    if os.path.exists("quiet/setting/prefixes.json"):
+        with open("prefixes.json", "r") as f:
+            prefix = f.read()
+        client.command_prefix = prefix
+
+    with open("prefixes.json", "w") as f:
+            f.write(client.command_prefix)
